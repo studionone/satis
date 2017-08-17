5d73a62a396331cf54f8d787cc66a83810a1834e
Devops <devops@studionone.com.au>
2017-08-16 22:32:33 -0400

    Fixing versions

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 echo "Rebuilding Satis..."
 touch code/satis.json
+php /root/satis/bin/satis build /root/satis/satis.json /root/satis/packages
 docker build -t satis:latest . && \
 docker stop satis && \
 docker rm satis && \
