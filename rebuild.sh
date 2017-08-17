b87df62223b9e479ea0dfe556bfbc9f9218f15d9
Devops <devops@studionone.com.au>
2017-08-16 22:09:21 -0400

    Adding new fork of Flint to repository and tidying up versions

---
+++ rebuild.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+echo "Rebuilding Satis..."
+touch code/satis.json
+docker build -t satis:latest . && \
+docker stop satis && \
+docker rm satis && \
+docker run --name satis -d --expose 80 -e VIRTUAL_HOST=composer.tools.studionone.io satis
+echo "Done"
